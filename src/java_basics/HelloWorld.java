7b459cf7b9296566e7c95bc9d53df70b661a1127
vinayak <vinayak.kumbhar@vxlsoftware.com>
2022-08-04 17:53:11 +0530

    added one classs and commiting add number to branch-x

--- src/java_basics/HelloWorld.java
+++ src/java_basics/HelloWorld.java
@@ -1,11 +1,21 @@
 package java_basics;
 
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 			System.out.println("This test program for Git testing.");
+			
+			AddNumbers add=new AddNumbers();
+			int x=add.sum(10,10);
+						
+			System.out.println("Sum of given numbers:"+x);
+			
+				
 	}
 
+	
+	
 }
